b9628604625f3014465525247a506ecede048fd7
unknown <quade.leonard@gmail.com>
2018-02-15 17:52:08 -0800

    Initial creation of drive class

---
+++ src/org/usfirst/frc/team5971/robot/Drive.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team5971.robot;
+
+/**
+ * @author Raider Robotics
+ *
+ */
+public class Drive {
+
+	/**
+	 * 
+	 */
+	public Drive() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
